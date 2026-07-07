68e19038d020831bbc132d8bd431e0c85cad3389
Rayhan Ahmadi <rayhanahmadi370@gmail.com>
2026-07-07 09:47:25 -0700

    student class: enroll in a course method

--- student.py
+++ student.py
@@ -12,3 +12,12 @@ class Student(Person):
     # Setter
     def set_student_id(self, student_id):
         self.__student_id = student_id
+
+ # Enroll in a course
+    def enroll_course(self, course_name):
+        if course_name not in self.__courses:
+            self.__courses[course_name] = None
+            print(f"Enrolled in {course_name}.")
+        else:
+            print("Student is already enrolled in this course.")
+
