7f83aafd0ae5a4c8d718552ba20f58f260a86c1e
Rayhan Ahmadi <rayhanahmadi370@gmail.com>
2026-07-06 23:50:13 -0700

    person class setters

--- person.py
+++ person.py
@@ -9,3 +9,13 @@ class Person:
 
         def get_age(self):
             return self.__age
+ # Setters
+    def set_name(self, name):
+        self.__name = name
+
+    def set_age(self, age):
+        if age > 0:
+            self.__age = age
+        else:
+            print("Age must be greater than 0.")
+
