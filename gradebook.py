708178f56cff156b9e08be60cc5ce32908bef126
Rayhan Ahmadi <rayhanahmadi370@gmail.com>
2026-07-07 18:00:54 -0700

    gradebook class: add student and remove student methods

--- gradebook.py
+++ gradebook.py
@@ -3,3 +3,13 @@ class Gradebook:
         self.__students = []
         self.__courses = []
 
+def add_student(self, student):
+    self.__students.append(student)
+    print("Student added.")
+
+def remove_student(self, student_id):
+    for student in self.__students:
+        if student.get_student_id() == student_id:
+            self.__students.remove(student)
+            print("Student removed.")
+            return
